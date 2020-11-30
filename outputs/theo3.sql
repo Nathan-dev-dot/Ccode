8cca0d46b4c002e664759b900694469bcc32d29c
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-11-30 13:23:07 +0100

    Stable version 2

--- outputs/theo3.sql
+++ outputs/theo3.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE theo;
-CREATE TABLE places(id integer auto_increment , name varchar(32) , address varchar(32) , map char(32) default null , primary key(id));
+CREATE DATABASE theo3;
+CREATE TABLE place(id integer auto_increment , name varchar(32) , address varchar(32) , map char(32) default null , primary key(id));
 CREATE TABLE drink(id integer auto_increment , name varchar(32) , category varchar(32) , primary key(id));
-CREATE TABLE _place_propose_drink(place integer , drink integer references drink(id), price integer , primary key(place,drink));
-CREATE TABLE room(id integer auto_increment , name varchar(32) , max_capacity integer , price_half_day integer , place integer , primary key(id));
+CREATE TABLE _place_propose_drink(place integer references place(id), drink integer references drink(id), price integer , primary key(place,drink));
+CREATE TABLE room(id integer auto_increment , name varchar(32) , max_capacity integer , price_half_day integer , place integer references place(id), primary key(id));
 CREATE TABLE booking(id integer auto_increment , nb_persons integer , price integer , date_booking date , time_slot timestamp , room integer references room(id), primary key(id));
 CREATE TABLE _booking_include_drink(booking integer references booking(id), drink integer references drink(id), primary key(booking,drink));
 CREATE TABLE equipment(id integer auto_increment , name varchar(32) , category varchar(32) , primary key(id));
